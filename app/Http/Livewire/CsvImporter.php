abaf59f1b0bddabf27e71c245e994742f03965c0
Sitt Aung <sittaung2021@gmail.com>
2022-08-08 10:51:22 +0630

    Add csv importer design

---
+++ app/Http/Livewire/CsvImporter.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class CsvImporter extends Component
+{
+    public function render()
+    {
+        return view('livewire.csv-importer');
+    }
+}
